b265dfa24af1ccf5816dc254f06221517deafb83
JackAttack-365 <142643773+JackAttack365@users.noreply.github.com>
2024-09-20 16:23:53 -0700

    Update Intake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Intake.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -28,5 +32,22 @@ public class Intake extends SubSystem {
     @Override
     public void update() {
 
+    }
+
+    public class GetFromTape implements Action {
+        private boolean initialized = false;
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            if (!initialized) {
+                initialized = true;
+            }
+
+            return true;
+        }
+    }
+
+    public Action getFromTape() {
+        return new GetFromTape();
     }
 }
