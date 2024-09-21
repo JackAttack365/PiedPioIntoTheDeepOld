6f4d82f2b6194384805e333b86db776c141eb57e
JackAttack-365 <142643773+JackAttack365@users.noreply.github.com>
2024-09-20 17:25:07 -0700

    pract -ice
    work on intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Intake.java
@@ -6,14 +6,22 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.Config;
 import org.piedmontpioneers.intothedeep.IntakeColorSensor;
+import org.piedmontpioneers.intothedeep.enums.Color;
 
 public class Intake extends SubSystem {
     DcMotor intake;
 
+    DcMotor leftExtendo;
+
     IntakeColorSensor colorSensor;
+
+    public final double EXTENDO_POWER = 0.8;
+
+    public boolean extendoOut;
 
     public Intake(Config config) {
         super(config);
@@ -25,8 +33,16 @@ public class Intake extends SubSystem {
 
     @Override
     public void init() {
+        leftExtendo = config.hardwareMap.get(DcMotor.class, "leftExt");
+
         intake = config.hardwareMap.get(DcMotor.class, "intake");
+
         colorSensor = new IntakeColorSensor(config.hardwareMap.get(ColorSensor.class, "colorSensor"));
+
+        intake.setDirection(DcMotorSimple.Direction.FORWARD);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        extendoOut = false;
     }
 
     @Override
@@ -36,14 +52,29 @@ public class Intake extends SubSystem {
 
     public class GetFromTape implements Action {
         private boolean initialized = false;
+        private boolean isExtendoOut = false;
+
+        private int extendoTargetPos = 100;
 
         @Override
         public boolean run(@NonNull TelemetryPacket packet) {
             if (!initialized) {
+                leftExtendo.setPower(EXTENDO_POWER);
+                intake.setPower(1);
                 initialized = true;
             }
 
-            return true;
+            if (leftExtendo.getCurrentPosition() >= extendoTargetPos && !isExtendoOut) {
+                leftExtendo.setPower(0);
+                isExtendoOut = true;
+            }
+
+            if (colorSensor.getDetection() == Color.RED) {
+                intake.setPower(0);
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 
